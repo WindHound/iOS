3d1eddbb36bf7f996ecb74c7c3ff012529c1b3fa
Dongnamu <danwoo0911@gmail.com>
2018-02-15 13:33:45 +0000

    Possible design for choosing races

---
+++ gpsTracker/Select_page.swift
@@ -0,0 +1,40 @@
+//
+//  Select_page.swift
+//  gpsTracker
+//
+//  Created by 신종훈 on 15/02/2018.
+//  Copyright © 2018 신종훈. All rights reserved.
+//
+
+import UIKit
+
+class Select_page: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        CGRect      buttonFrame = button.frame;
+//        buttonFrame.size = CGSizeMake(150, 70);
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func unwindToCategoryList(segue:UIStoryboardSegue) { }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
