b6ab858c6aa3d06836f6348ef4d54bd50ff0044b
Dongnamu <danwoo0911@gmail.com>
2018-02-21 03:29:42 +0000

    Upcoming and History drop down table for championship has been developed

--- gpsTracker/Championship.swift
+++ gpsTracker/Championship.swift
@@ -7,33 +7,78 @@
 //
 
 import UIKit
+import Foundation
 
-class Championship: UIViewController, UITableViewDelegate, UITableViewDataSource {
+var upcoming_champ : [String] = []
+var history_champ : [String] = []
+
+class Championship: UITableViewController{
     
-    var champ_list : [String] = []
+    
+    @IBOutlet weak var outside_table: UITableView!
+    @IBOutlet weak var upcoming_table: UITableView!
+    @IBOutlet weak var history_table: UITableView!
+    
+    private var UpcomingCellExpanded : Bool = true
+    
+    private var HistoryCellExpanded : Bool = false
+
+    var Up = Upcoming()
+
+    var His = History()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        champ_list.append("Bristol")
+        for i in 1...10 {
+            upcoming_champ.append("Championship\(i)")
+        }
+        
+        for i in 1...12 {
+            history_champ.append("Championship\(i)")
+        }
+        
+        upcoming_table.delegate = Up
+        upcoming_table.dataSource = Up
+        history_table.delegate = His
+        history_table.dataSource = His
+
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return(champ_list.count)
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if tableView == outside_table {
+            return(2)
+        }
+        if tableView == upcoming_table {
+            return upcoming_champ.count
+        }
+        if tableView == history_table {
+            return history_champ.count
+        }
+        return 0
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
-        cell.textLabel?.text = champ_list[indexPath.row]
-        
-        return(cell)
-    }
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        UITableViewCell cell = nil
+//
+//        if tableView == Upcoming_table {
+//            cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Upcoming_champ")
+//            cell.textLabel?.text = upcoming_champ[indexPath.row]
+//        }
+//        if tableView == History_table {
+//            cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "History_champ")
+//            cell.textLabel?.text = history_champ[indexPath.row]
+//        }
+//
+//        return cell
+//    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let destination = segue.identifier
@@ -44,10 +89,51 @@ class Championship: UIViewController, UITableViewDelegate, UITableViewDataSource
         }
     }
     
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        performSegue(withIdentifier: "Upcoming_event_detail", sender: self)
-//        tableView.reloadData()
-//    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView == outside_table {
+            if indexPath.row == 0 {
+                if !UpcomingCellExpanded {
+                    UpcomingCellExpanded = true
+                    HistoryCellExpanded = false
+                }
+            }
+            
+            if indexPath.row == 1 {
+                if !HistoryCellExpanded {
+                    UpcomingCellExpanded = false
+                    HistoryCellExpanded = true
+                }
+            }
+            
+            outside_table.beginUpdates()
+            outside_table.endUpdates()
+
+            outside_table.deselectRow(at: indexPath, animated: true)
+        }
+        
+        
+    
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if tableView == outside_table {
+            if indexPath.row == 0 {
+                if UpcomingCellExpanded {
+                    return (self.outside_table.bounds.height - self.navigationController!.navigationBar.frame.size.height - (self.tabBarController?.tabBar.frame.size.height)!)
+                } else {
+                    return 50
+                }
+            }
+            if indexPath.row == 1 {
+                if HistoryCellExpanded {
+                    return (self.outside_table.bounds.height - self.navigationController!.navigationBar.frame.size.height - (self.tabBarController?.tabBar.frame.size.height)!)
+                } else {
+                    return 50
+                }
+            }
+        }
+        return 50
+    }
     
 
     
@@ -67,4 +153,42 @@ class Championship: UIViewController, UITableViewDelegate, UITableViewDataSource
     
     
 
+}
+
+class Upcoming : NSObject, UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return upcoming_champ.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Upcoming_champ", for: indexPath)
+        
+        cell.textLabel?.text = upcoming_champ[indexPath.row]
+        return cell
+    }
+    
+}
+
+class History : NSObject, UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return history_champ.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "History_champ", for: indexPath)
+        
+        cell.textLabel?.text = history_champ[indexPath.row]
+        return cell
+    }
+    
 }
--- gpsTracker/Event.swift
+++ gpsTracker/Event.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class Event: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class Event: UITableViewController{
 
     @IBOutlet weak var toolbar: UIToolbar!
     var event_list : [String] = []
@@ -25,11 +25,11 @@ class Event: UIViewController, UITableViewDelegate, UITableViewDataSource {
         // Dispose of any resources that can be recreated.
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return(event_list.count)
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
         cell.textLabel?.text = event_list[indexPath.row]
         
