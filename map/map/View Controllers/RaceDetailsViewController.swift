bc2091b77819fcf1cd8f69961c104d89acd11131
msa184 <ms16473@my.bristol.ac.uk>
2018-03-14 06:41:36 +0000

    Map 0.86 syn

--- map/map/View Controllers/RaceDetailsViewController.swift
+++ map/map/View Controllers/RaceDetailsViewController.swift
@@ -8,83 +8,83 @@ class RaceDetailsViewController: UIViewController, UICollectionViewDataSource, U
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
     
     var race: Race!
     
-//Map
-        @IBOutlet weak var mapView: MKMapView!
- /*
-        //Starting Position - DEMO - SHOULD BE FIRST GPS LOCATION
-        let initialLocation = CLLocation(latitude: 51.455896, longitude: -2.603118)
-        
-        //Radius of Map in meters
-        let regionRadius: CLLocationDistance = 1000
-        func centerMapOnLocation(location: CLLocation) {
-            let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
-                                                                      regionRadius, regionRadius)
-            mapView.setRegion(coordinateRegion, animated: true)
-        }
+    //Map Init
+    /*
+     //Starting Position - DEMO - SHOULD BE FIRST GPS LOCATION
+     let initialLocation = CLLocation(latitude: 51.455896, longitude: -2.603118)
+     
+     //Radius of Map in meters
+     let regionRadius: CLLocationDistance = 1000
+     func centerMapOnLocation(location: CLLocation) {
+     let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+     regionRadius, regionRadius)
+     mapView.setRegion(coordinateRegion, animated: true)
+     }
+     
+     
+     //Location Permission -- Also in .plist
+     let locationManager = CLLocationManager()
+     func checkLocationAuthorizationStatus() {
+     if CLLocationManager.authorizationStatus() == .authorizedWhenInUse
+     {
+     mapView.showsUserLocation = true
+     } else
+     {
+     locationManager.requestWhenInUseAuthorization()
+     }
+     }
+     
+     override func viewDidAppear(_ animated: Bool)
+     {
+     super.viewDidAppear(animated)
+     checkLocationAuthorizationStatus()
+     }
+     
+     */
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        //Initialise map
+        //centerMapOnLocation(location: initialLocation)
+        configureView()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
     
     
-        //Location Permission -- Also in .plist
-        let locationManager = CLLocationManager()
-        func checkLocationAuthorizationStatus() {
-            if CLLocationManager.authorizationStatus() == .authorizedWhenInUse
-            {
-                mapView.showsUserLocation = true
-            } else
-            {
-                locationManager.requestWhenInUseAuthorization()
-            }
-        }
-        
-        override func viewDidAppear(_ animated: Bool)
-        {
-            super.viewDidAppear(animated)
-            checkLocationAuthorizationStatus()
-        }
-        
- */
-        override func viewDidLoad()
-        {
-            super.viewDidLoad()
-            //Initialise map
-            //centerMapOnLocation(location: initialLocation)
-            configureView()
-            // Do any additional setup after loading the view, typically from a nib.
-        }
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
     
-        override func didReceiveMemoryWarning()
-        {
-            super.didReceiveMemoryWarning()
-            // Dispose of any resources that can be recreated.
-        }
+    //LeaderboardImageArray
+    var LeaderboardImageArray = [UIImage(named: "sailboat-1"), UIImage(named: "sailboat-2"), UIImage(named: "sailboat-3"), UIImage(named: "sailboat-4"), UIImage(named: "sailboat-5"), UIImage(named: "sailboat-6")]
     
-        
-        //LeaderboardImageArray
-        var LeaderboardImageArray = [UIImage(named: "sailboat-1"), UIImage(named: "sailboat-2"), UIImage(named: "sailboat-3"), UIImage(named: "sailboat-4"), UIImage(named: "sailboat-5"), UIImage(named: "sailboat-6")]
-        
-        //DataImageArray -- NOT USED
-        var DataImageArray = [UIImage(named: "sailboat-1"), UIImage(named: "sailboat-2"), UIImage(named: "sailboat-3"), UIImage(named: "sailboat-4"), UIImage(named: "sailboat-5"), UIImage(named: "sailboat-6")]
-        
-        //Leaderboard
-        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
-        {
-            return LeaderboardImageArray.count
-        }
-        
-        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
-        {
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "LeaderboardCollectionViewCell", for: indexPath)
-                as! LeaderboardCollectionViewCell
-            
-            cell.LeaderboardImage.image = LeaderboardImageArray[indexPath.row]
-            
-            return cell
-        }
+    //DataImageArray -- NOT USED
+    var DataImageArray = [UIImage(named: "sailboat-1"), UIImage(named: "sailboat-2"), UIImage(named: "sailboat-3"), UIImage(named: "sailboat-4"), UIImage(named: "sailboat-5"), UIImage(named: "sailboat-6")]
     
-//Race Replay
+    //Leaderboard
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        return LeaderboardImageArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "LeaderboardCollectionViewCell", for: indexPath)
+            as! LeaderboardCollectionViewCell
+        
+        cell.LeaderboardImage.image = LeaderboardImageArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //Race Replay
     private func configureView()
     {
         let distance = Measurement(value: race.distance, unit: UnitLength.meters)
@@ -93,14 +93,14 @@ class RaceDetailsViewController: UIViewController, UICollectionViewDataSource, U
         let formattedDate = FormatDisplay.date(race.timestamp)
         let formattedTime = FormatDisplay.time(seconds)
         let formattedSpeed = FormatDisplay.speed(distance: distance,
-                                               seconds: seconds,
-                                               outputUnit: UnitSpeed.nauticalMiles)
+                                                 seconds: seconds,
+                                                 outputUnit: UnitSpeed.nauticalMiles)
         
         distanceLabel.text = "Distance:  \(formattedDistance)"
         dateLabel.text = formattedDate
         timeLabel.text = "Time:  \(formattedTime)"
         speedLabel.text = "Speed:  \(formattedSpeed)"
-
+        
         loadMap()
     }
     
@@ -143,10 +143,10 @@ class RaceDetailsViewController: UIViewController, UICollectionViewDataSource, U
         }
         
         let coords: [CLLocationCoordinate2D] = locations.map
-            { location in
+        { location in
             let location = location as! Location
             return CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
-            }
+        }
         return MKPolyline(coordinates: coords, count: coords.count)
     }
     
@@ -157,14 +157,14 @@ class RaceDetailsViewController: UIViewController, UICollectionViewDataSource, U
             locations.count > 0,
             let region = mapRegion()
             else
-            {
-                let alert = UIAlertController(title: "Error",
-                                              message: "Sorry, this race has no locations saved",
-                                              preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "OK", style: .cancel))
-                present(alert, animated: true)
-                return
-            }
+        {
+            let alert = UIAlertController(title: "Error",
+                                          message: "Sorry, this race has no locations saved",
+                                          preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel))
+            present(alert, animated: true)
+            return
+        }
         
         mapView.setRegion(region, animated: true)
         mapView.mapType = MKMapType.satellite
